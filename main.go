a5c7a30b2c0faa17cbe274e8ee5907f68a2b7a67
Emiliano2xG <emilianoguillenguillen.23@gmail.com>
2026-08-29 22:10:18 -0600

    Middleware primer parcial

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"monolito_tarea_1/controlador"
+	"monolito_tarea_1/middleware"
 	"monolito_tarea_1/modelo"
 )
 
@@ -15,17 +16,22 @@ func main() {
 	}
 	defer conexion.Close()
 
+	// Asi no tengo que repetir los middleware en cada ruta
+	conMiddleware := func(manejador http.HandlerFunc) http.HandlerFunc {
+		return middleware.RegistrarPeticion(middleware.EvitarErrores(manejador))
+	}
+
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", conMiddleware(func(w http.ResponseWriter, r *http.Request) {
 		controlador.MostrarInicio(w, r, conexion)
-	})
-	http.HandleFunc("/tareas", func(w http.ResponseWriter, r *http.Request) {
+	}))
+	http.HandleFunc("/tareas", conMiddleware(func(w http.ResponseWriter, r *http.Request) {
 		controlador.AgregarTarea(w, r, conexion)
-	})
-	http.HandleFunc("/tareas/completar", func(w http.ResponseWriter, r *http.Request) {
+	}))
+	http.HandleFunc("/tareas/completar", conMiddleware(func(w http.ResponseWriter, r *http.Request) {
 		controlador.MarcarTarea(w, r, conexion)
-	})
+	}))
 
 	fmt.Println("Servidor en http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))

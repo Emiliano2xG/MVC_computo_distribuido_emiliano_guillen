e73dd3ad8a2204ea33b129427f62ad13281d4180
Emiliano2xG <emilianoguillenguillen.23@gmail.com>
2026-08-29 22:29:02 -0600

    Middleware primer parcial docker

--- main.go
+++ main.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"monolito_tarea_1/controlador"
-	"monolito_tarea_1/middleware"
+	"monolito_tarea_1/filtros"
 	"monolito_tarea_1/modelo"
 )
 
@@ -16,23 +17,34 @@ func main() {
 	}
 	defer conexion.Close()
 
-	// Asi no tengo que repetir los middleware en cada ruta
-	conMiddleware := func(manejador http.HandlerFunc) http.HandlerFunc {
-		return middleware.RegistrarPeticion(middleware.EvitarErrores(manejador))
+	// El nombre me sirve para saber cual worker contesto cuando hago las pruebas
+	nombreDelWorker := os.Getenv("NOMBRE_WORKER")
+	if nombreDelWorker == "" {
+		nombreDelWorker = "worker"
+	}
+
+	puerto := os.Getenv("PORT")
+	if puerto == "" {
+		puerto = "8080"
+	}
+
+	// Asi no tengo que repetir los filtros en cada ruta
+	conFiltros := func(manejador http.HandlerFunc) http.HandlerFunc {
+		return filtros.RegistrarPeticion(filtros.EvitarErrores(manejador))
 	}
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	http.HandleFunc("/", conMiddleware(func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", conFiltros(func(w http.ResponseWriter, r *http.Request) {
 		controlador.MostrarInicio(w, r, conexion)
 	}))
-	http.HandleFunc("/tareas", conMiddleware(func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/tareas", conFiltros(func(w http.ResponseWriter, r *http.Request) {
 		controlador.AgregarTarea(w, r, conexion)
 	}))
-	http.HandleFunc("/tareas/completar", conMiddleware(func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/tareas/completar", conFiltros(func(w http.ResponseWriter, r *http.Request) {
 		controlador.MarcarTarea(w, r, conexion)
 	}))
 
-	fmt.Println("Servidor en http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Printf("Worker de %s escuchando en el puerto %s\n", nombreDelWorker, puerto)
+	log.Fatal(http.ListenAndServe(":"+puerto, nil))
 }
